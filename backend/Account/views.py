89ce91c3ce274122529d32775c868d3685ab23d5
Abdulnaser Sheikh <abdulnsheikh@hotmail.com>
2022-09-22 14:47:47 -0500

    added the views for the authentication

--- backend/Account/views.py
+++ backend/Account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+ 
+
+########### register here #####################################
+def register(request):
+    pass
+
+################ login forms###################################################
+def login(request):
+    pass
