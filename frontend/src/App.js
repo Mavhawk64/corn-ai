33ae16768003862cf5426343a27735c2629a1172
Chris Fuchs <cfuchs@unomaha.edu>
2022-10-16 17:58:03 -0500

    Authentication pages added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useContext } from "react";
 import './App.css';
 import HomePage from './components/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Training from './components/training';
+import AuthForm from "./components/Auth/AuthForm";
+import AuthContext from "./store/auth-context";
 
 function App() {
+    const authCtx = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Train" element={<Training />} />
+        <Route path="/" element={authCtx.isLoggedIn ? <HomePage /> : <AuthForm />} />
+        <Route path="/train" element={authCtx.isLoggedIn ? <Training /> : <AuthForm />} />
+        <Route path="/auth" element={authCtx.isLoggedIn ? <AuthForm /> : <HomePage />} />
+        <Route path="*" element={authCtx.isLoggedIn ? <HomePage /> : <AuthForm />} />
       </Routes>
     </Router>
   );
