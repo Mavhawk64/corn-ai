10366550514b638ac077e151923d689cc5d7b902
Chris Fuchs <cfuchs@unomaha.edu>
2022-11-11 11:53:25 -0600

    Change class to className

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,26 +6,26 @@ const HomePage = () => {
 
     return (
         <div>
-            <div class="row">
-                <div class="column7 round shaded_light_norm">
-                    <div class="fill round">
+            <div className="row">
+                <div className="column7 round shaded_light_norm">
+                    <div className="fill round">
                         <img src={require('../inc/img/AI_hero.jpg')} alt='AI'/>
                     </div>
                 </div>
-                <div class="column3">
-                    <div class='row p_topL'>
-                        <div class="column5"></div>
+                <div className="column3">
+                    <div className='row p_topL'>
+                        <div className="column5"></div>
                     </div>
                 </div>
             </div>
-            <div class="row p_top">
-                <div class="column3">
-                    <h1 class="mar_left">AI for Agriculture!</h1>
+            <div className="row p_top">
+                <div className="column3">
+                    <h1 className="mar_left">AI for Agriculture!</h1>
                 </div>
-                <div class="column7">
-                    <div class="row">
-                        <div class="column4"></div>
-                        <div class="bkg_Color1 round mar_right p">
+                <div className="column7">
+                    <div className="row">
+                        <div className="column4"></div>
+                        <div className="bkg_Color1 round mar_right p">
                             <h1>SEE THE FUTURE!</h1>
                         </div>
                     </div>
--- frontend/src/components/training.js
+++ frontend/src/components/training.js
@@ -5,30 +5,30 @@ const Training = () => {
 
     return (
         <div>
-            <div class="row header">
-                <div class="header-background row">
-                    <button class="bkg_ColorBlack o_btn o_btnM" type="button">
+            <div className="row header">
+                <div className="header-background row">
+                    <button className="bkg_ColorBlack o_btn o_btnM" type="button">
                         <b>Previous</b>
                     </button>
-                    <button class="bkg_ColorWhite o_btn o_btnM" type="button">
+                    <button className="bkg_ColorWhite o_btn o_btnM" type="button">
                         <b>Next</b>
                     </button>
                 </div>
             </div>
-            <div class="row">
-                <div class="column6 p_topL fill p_leftL">
+            <div className="row">
+                <div className="column6 p_topL fill p_leftL">
                     <img
                         src='https://sfo3.digitaloceanspaces.com/csci4970-agro-ai-images/AI_Images/original_corn_pics/images_handheld/DSC00033.JPG'
                         height={600}
                         width={100} alt="placeholder"></img>
                 </div>
-                <div class="column4"></div>
+                <div className="column4"></div>
             </div>
-            <div class="row header">
-                <button class="bkg_ColorHealthy o_btn o_btnXL" type="button">
+            <div className="row header">
+                <button className="bkg_ColorHealthy o_btn o_btnXL" type="button">
                     <b>Healthy</b>
                 </button>
-                <button class="bkg_ColorUnhealthy o_btn o_btnXL mar_left" type="button">
+                <button className="bkg_ColorUnhealthy o_btn o_btnXL mar_left" type="button">
                     <b>Unhealthy</b>
                 </button>
             </div>
